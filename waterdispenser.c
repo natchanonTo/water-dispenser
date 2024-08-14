9ad46e67d4a4edf6993e3f290a9d31ccfe46f9c0
natchanonTo <natchanontomudbarung@gmail.com>
2024-08-14 14:15:25 +0700

    update water dispenser 1.0.2

--- waterdispenser.c
+++ waterdispenser.c
@@ -9,7 +9,7 @@ int main () {
     printf("Water dispenser\n");
     printf("Input your money\n");
     scanf("%d",&money);
-    printf("Money %d\n",money);
+    printf("Money %d bart\n",money);
 
     //Input water type
     printf("Choose your water\n");
@@ -30,24 +30,61 @@ int main () {
             printf("Input more money\n");
             scanf("%d",&extramoney);
             money += extramoney;
+            
         }
-        
+        money -= cost;
+        printf("Withdraw money %d bart",money);
+
     }
     else if (watertype==2)
     {
-        /* code */
+        cost = 30;
+        printf("You choose cola\n");
+        while (money<cost)
+        {
+            printf("Input more money\n");
+            scanf("%d",&extramoney);
+            money += extramoney;
+            
+        }
+        money -= cost;
+        printf("Withdraw money %d bart",money);
+
     }
     else if (watertype==3)
     {
-        /* code */
+        cost = 30;
+        printf("You choose cola\n");
+        while (money<cost)
+        {
+            printf("Input more money\n");
+            scanf("%d",&extramoney);
+            money += extramoney;
+            
+        }
+        money -= cost;
+        printf("Withdraw money %d bart",money);
+
     }   
     else if (watertype==4)
     {
-        /* code */
+        cost = 40;
+        printf("You choose cola\n");
+        while (money<cost)
+        {
+            printf("Input more money\n");
+            scanf("%d",&extramoney);
+            money += extramoney;
+            
+        }
+        money -= cost;
+        printf("Withdraw money %d bart",money);
+
     }
     else
     {
-        
+        printf("Invalid water type\n");
+        printf("Withdraw money %d bart\n",money);
     }
     return 0 ;
 
